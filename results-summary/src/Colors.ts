a6fa147fa15ca9da064bf6a5d00d44ae856c1761
הדר שושן <hadarhadar888@gmail.com>
2024-07-12 18:42:31 +0300

    create result component style

---
+++ results-summary/src/Colors.ts
@@ -0,0 +1,20 @@
+export const colors = {
+    primary: {
+        lightRed: 'hsl(0, 100%, 67%)',
+        orangeyYello: 'hsl(39, 100%, 56%)',
+        greenTeal: 'hsl(166, 100%, 37%)',
+        cobaltBlue: 'hsl(234, 85%, 45%)',
+    },
+    gradients: {
+        lightSlate: 'hsl(252, 100%, 67%)',
+        lightRoyal: 'hsl(241, 81%, 54%)',
+        violet: 'hsla(256, 72%, 46%, 1)',
+        persian: 'hsla(241, 72%, 46%, 0)',
+    },
+    Neutral: {
+        white: 'hsl(0, 0%, 100%)',
+        paleBlue: 'hsl(221, 100%, 96%)',
+        lightLavender: 'hsl(241, 100%, 89%)',
+        darkGrayBlue: 'hsl(224, 30%, 27%)',
+    }
+}
--- results-summary/src/Result.tsx
+++ results-summary/src/Result.tsx
@@ -1,22 +1,45 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+import { colors } from "./Colors";
+import styled from "@emotion/styled";
+
+const GridContainer = styled(Grid)({
+    height: '100%',
+})
 
 const Result = () => {
     return (
-        <Box>
-            <Grid container direction="column" alignItems="center">
+        <Box height="300px" padding={5}>
+            <GridContainer container direction="column" alignItems="center" justifyContent="space-between">
                 <Grid item>
-                    Result
+                    <Typography variant="body1" fontWeight="bold" color={colors.Neutral.lightLavender}>
+                        Your Result
+                    </Typography>
+                </Grid>
+                <Grid item style={{background: "linear-gradient(hsla(256, 72%, 46%, 1), hsla(241, 72%, 46%, 0))", height: '150px', width: '150px', borderRadius: '100px'}}>
+                    <GridContainer container direction="column" justifyContent="center" alignItems="center">
+                        <Grid item>
+                            <Typography variant="h2" fontWeight="bold" color={colors.Neutral.white}>
+                                76 
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="body2" color={colors.Neutral.lightLavender}>
+                                of 100
+                            </Typography>
+                        </Grid>
+                    </GridContainer>
                 </Grid>
                 <Grid item>
-                    76 out of 100
+                    <Typography variant="h6" fontWeight="bold" color={colors.Neutral.white}>
+                        Great
+                    </Typography>
                 </Grid>
                 <Grid item>
-                    Great
+                    <Typography variant="subtitle2" align="center" color={colors.Neutral.lightLavender}>
+                        you score higher than 65% of the people who have taken these tests.
+                    </Typography>
                 </Grid>
-                <Grid item>
-                    you score higher than 65% of the people who have taken these tests.
-                </Grid>
-            </Grid>
+            </GridContainer>
         </Box>
     );
 }
